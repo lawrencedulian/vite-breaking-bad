013e43f057b430bde7241ed4d43d70e9da193060
lawrencedulian <lawrenceedulian@gmail.com>
2022-11-10 15:13:14 +0100

    AppHeader Done

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<template>
+  <AppHeader />
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: "AppHeader",
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <header class="d-flex align-items-center p-1">
+        <h1>
+            Breaking Bad Api
+        </h1>
+    </header>
+</template>
+
